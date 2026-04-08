466ee6c3f7668c39b97a3a0227e41df2810a3e64
Renzo Ramos <renzoramosivan@gmail.com>
2026-04-08 17:47:26 +0200

    feat: crear barrel de constantes del frontend

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,10 @@
+/**
+ * Barrel export de todas las constantes de la aplicación
+ * Permite importar constantes desde un único punto de entrada
+ * 
+ * @example
+ * import { WINDOW_LIMIT, TONES, TONE_LABELS } from '../constants';
+ */
+
+export * from './limits';
+export * from './tones';
